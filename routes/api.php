1fc66b18f79096afe9ef988f0a86d9fb6261e685
KynanMuzha <muhammadkynanmuzhaffar@gmail.com>
2025-12-07 14:55:16 +0700

    auth update

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\http\Controllers\Api\NoteController;
+use App\Http\Controllers\Api\NoteController;
+use App\Http\Controllers\AuthController;
 
+// AUTH
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+});
+
+// NOTES
 Route::get('/notes/trash', [NoteController::class, 'trash']);
 Route::get('/notes/trash/{id}', [NoteController::class, 'trashed']);
 Route::delete('/notes/{id}/force', [NoteController::class, 'forceDelete']);
